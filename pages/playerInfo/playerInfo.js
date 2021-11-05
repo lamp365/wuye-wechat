cb5095cb29b0643ce46444e4dfe4a2f144b7dc2d
hanyi365 <791845283@qq.com>
2021-11-06 02:42:19 +0800

    finish playerinfo

--- pages/playerInfo/playerInfo.js
+++ pages/playerInfo/playerInfo.js
@@ -1,18 +1,34 @@
 // pages/playerInfo/playerInfo.js
+import {Config} from "../../utils/config.js";
+import {Base} from '../../utils/base.js';
+const utils = require('../../utils/util.js');
+
+var BaseObj = new Base();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    baseUrl: Config.baseUrl, //图片路径
+    showLoading: true,
+    player_id:'', //选手ID
+    subject_id:'', //项目ID
+    player:[], //选手信息
+    datasNumber: [], //数量接口
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var player_id = options.player_id;
+    this.setData({
+      player_id:player_id
+    })
 
+    this.getplayerInfo();
   },
 
   /**
@@ -28,7 +44,46 @@ Page({
   onShow: function () {
 
   },
+  
+  getplayerInfo:function(){
+    var that = this;
+    var parame = {
+      url:Config.playerInfo,
+      data:{player_id:this.data.player_id},
+      type:'post',
+      sCallback:function(res){
+        if(res.content){
+          res.content = utils.imageUrlReplace(res.content);
+        }
+        that.setData({
+          player: res, 
+          showLoading: false
+        });
+      }
+    };
+    BaseObj.request(parame);
+  },
 
+  playerSetVotes:function(e){
+    var that = this;
+    var player_id = this.data.player_id;
+    var subject_id = e.currentTarget.dataset.subjectid;
+    var parame = {
+      url:Config.submitVote,
+      data:{player_id:player_id,subject_id:subject_id},
+      type:'post',
+      needShowMessage:true,
+      sCallback:function(res){
+         //更新节点数据
+         var player = that.data.player;
+        player.votes += 1;
+         that.setData({
+           player:player
+         })
+      }
+    }; 
+    BaseObj.request(parame);
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
