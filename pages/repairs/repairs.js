7936c7891460f4386f174d692251642fa0f3a7df
hanyi365 <791845283@qq.com>
2021-11-07 02:03:36 +0800

    repair

--- pages/repairs/repairs.js
+++ pages/repairs/repairs.js
@@ -1,11 +1,24 @@
 // pages/repairs/repairs.js
+import {Config} from "../../utils/config.js";
+import {Base} from '../../utils/base.js';
+
+var BaseObj = new Base();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    array: [], //全部的类型数组
+    flag: 0, //选中的下标
+    index: 0, //选中的下标
+    arrayName: [], //筛选出来的数组
+    text: '', //输入的问题描述
+    temp: [], //上传的图片
+    address: '', //获取默认地址
+    wuyeName: '', //物业名字
+    showLoading: true,
+    imgs: [], //评价上传后的图片
   },
 
   /**
--- pages/subjectInfo/subjectInfo.js
+++ pages/subjectInfo/subjectInfo.js
@@ -152,6 +152,9 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: Config.applet,
+      path: 'pages/start/start'
+    };
   }
 })
