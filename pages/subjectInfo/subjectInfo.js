4b31842d320bbffab1ebc6a58a2a77e688baeeb9
hanyi365 <791845283@qq.com>
2021-11-07 00:53:43 +0800

    finish vote

--- pages/subjectInfo/subjectInfo.js
+++ pages/subjectInfo/subjectInfo.js
@@ -1,18 +1,34 @@
 // pages/subjectInfo/subjectInfo.js
+import {Config} from '../../utils/config.js';
+import {Base} from '../../utils/base.js';
+var util = require('../../utils/util.js');
+var BaseObj = new Base();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    baseUrl: Config.baseUrl, //图片
+    showLoading: true, //加载动画
+    masktop: '', //是否显示商品分类
+    currPage: 1, //当前页数
+    totalPage: '', //总页数
+    subjectData: [], //项目信息
+    playerData: [], //选手数据
+    contents:'', //活动内容
+    isHideContent: true, //判断活动规则关闭 开关
+   
+    showModal: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var subject_id = options.subject_id;
+    this.getSubjectInfo(subject_id);
   },
 
   /**
@@ -28,7 +44,82 @@ Page({
   onShow: function () {
 
   },
+  getSubjectInfo:function(subject_id){
+    var that = this;
+    var parame = {
+      url:Config.oneSubject,
+      data:{subject_id:subject_id},
+      type:'post',
+      sCallback:function(res){
+        that.setData({
+          subjectData:res,
+          playerData:res.playerList,
+          showLoading:false,
+          content:util.imageUrlReplace(res.content)
+        });
+      }
+    }
+    BaseObj.request(parame);
+  },
 
+  bindtapPlayerInfo:function(e){
+    var player_id = e.currentTarget.dataset.playerid;
+    wx.navigateTo({
+      url: `../playerInfo/playerInfo?player_id=${player_id}`,
+    })
+  },
+  catchtapStopMaopao:function(){
+    return false;
+  },
+
+  bindtapSubmitVote:function(e){
+    var that = this;
+    var player_id = e.currentTarget.dataset.playerid;
+    var subject_id = e.currentTarget.dataset.subjectid;
+    var index = e.currentTarget.dataset.index;
+    var parame = {
+      url:Config.submitVote,
+      data:{player_id:player_id,subject_id:subject_id},
+      type:'post',
+      needShowMessage:true,
+      sCallback:function(res){
+         //更新节点数据
+         var player = that.data.playerData;
+         for(let i in player){
+             if(i == index){
+               player[i].votes =  player[i].votes * 1+1;
+             }
+         }
+         that.setData({
+          playerData:player
+         })
+      }
+    }; 
+    BaseObj.request(parame);
+  },
+
+  bindtapSearch:function(){
+    wx.navigateTo({
+      url: '../search/search?type=1', //1是查询选手
+    })
+  },
+  bindClose:function(){
+    this.setData({
+      isHideContent:true,
+    });
+  },
+  //查看详情规则
+  bindtapSubjectContent:function(){
+    this.setData({
+      showLoading: false,
+      isHideContent:false,
+    });    
+  },
+  bindtapAllSubject:function(){
+    wx.navigateTo({
+      url: '../subjectList/subjectList',
+    })
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
--- pages/subjectList/subjectList.js
+++ pages/subjectList/subjectList.js
@@ -1,18 +1,27 @@
 // pages/subjectList/subjectList.js
+import {Config} from '../../utils/config.js';
+import {Base} from '../../utils/base.js';
+var BaseObj = new Base();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    baseUrl: Config.baseUrl, //图片路径
+    allSubject: [], //项目数据
+    number:0,
+    totalPage: '', //总页数
+    currPage: 1,  //当前页数
+    showLoading: true, //是否显示加载
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getAllSubjectList();
   },
 
   /**
@@ -29,6 +38,26 @@ Page({
 
   },
 
+  getAllSubjectList:function(){
+    var that = this;
+    var parame = {
+      url:Config.subject,
+      sCallback:function(res){
+        that.setData({
+          allSubject:res,
+          showLoading: false
+        });
+      }
+    }
+    BaseObj.request(parame);
+  },
+
+  bindtapSubjectInfo:function(e){
+    var subject_id =  e.currentTarget.dataset.subjectid;
+    wx.navigateTo({
+      url: '../subjectInfo/subjectInfo?subject_id='+subject_id,
+    })
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
@@ -61,6 +90,9 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: Config.applet,
+      path: 'pages/start/start'
+    };
   }
 })
